72a9bb9cb5b526b9d4836f97cbe712117cde58b8
Alex Mazheika <alex.mazheika@gmail.com>
2023-06-15 18:20:16 +0200

    implemented a trial CNN Alex_model and prediction on top of it and merged with prev

--- autism/api/fast.py
+++ autism/api/fast.py
@@ -2,44 +2,48 @@ import numpy as np
 import cv2
 from fastapi import FastAPI, File, UploadFile
 import requests
-from autism.ml_logic import main, model, preprocessor
+from tensorflow import keras
 
 
 app = FastAPI()
 
 @app.post('/upload_image')
-async def receive_image(img: UploadFile=File(...)):   #################################### WHATS GOING ON HERE ?
+async def receive_image(img: UploadFile=File(...)):
     ### Receiving and decoding the image
     contents = await img.read()
 
     nparr = np.fromstring(contents, np.uint8)
     cv2_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR) # type(cv2_img) => numpy.ndarray
 
-    ### Do cool stuff with your image.... For example face detection
-    annotated_img = annotate_face(cv2_img) ############################################### WHATS GOING ON HERE ? DOESNT CONCERN US ?
+    ### PREPOCESSING   CHANGE NAME WITH ACTUAL FUNCTION NAME
+    prepoc_img = preprocessor(cv2_img)
 
     ### Encoding and responding with the image
-    im = cv2.imencode('.png', annotated_img)[1] # extension depends on which format is sent from Streamlit
-    return Response(content=im.tobytes(), media_type="image/png")
+    im = cv2.imencode('.png', prepoc_img)[1] # extension depends on which format is sent from Streamlit
+    return requests.Response(content=im.tobytes(), media_type="image/png")
 
 
 
 
-@app.post('/predict')
-async def receive_image(img: UploadFile=File(...)):   #################################### WHATS GOING ON HERE ?
-    ### Receiving and decoding the image
-    contents = await img.read()
+@app.get('/predict')
+async def predict(prepoc_img):
+    model = keras.model.load_model('./model/model_homemade.h5')
+    pred = model.predict(prepoc_img)
+    return pred
 
-    nparr = np.fromstring(contents, np.uint8)
-    cv2_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR) # type(cv2_img) => numpy.ndarray
 
-    ### Do cool stuff with your image.... For example face detection
-    annotated_img = annotate_face(cv2_img) ############################################### WHATS GOING ON HERE ? DOESNT CONCERN US ?
 
-    ### Encoding and responding with the image
-    im = cv2.imencode('.png', annotated_img)[1] # extension depends on which format is sent from Streamlit
-    return Response(content=im.tobytes(), media_type="image/png")
 
+
+@app.get("/")
+def root():
+    """
+    Root endpoint for the API.
+    """
+    return {"greeting": "Hello"}
+
+
+from autism.ml_logic import main, model, preprocessor
 @app.post('/test')
 def receive_image(img: UploadFile=File(...)):
     im_resized = preprocessor.resize_58x64(img)
